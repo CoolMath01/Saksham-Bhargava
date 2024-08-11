9d49165945be9d00bfabcd53ec8a931a73442600
Saksham <100786485+SakBha@users.noreply.github.com>
2024-08-11 19:09:23 +0530

    addition

--- vite-project/src/sections/Hero/Hero.jsx
+++ vite-project/src/sections/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/Design uten navn.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
+import GFGLight from '../../assets/GFG_Light.png';
+import GFGDark from '../../assets/GFG_dark.png';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
@@ -15,7 +15,7 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const GFGIcon = theme === 'light' ? GFGLight : GFGDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -42,10 +42,10 @@ function Hero() {
         </h1>
         <h2>Software Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          <a href="https://www.geeksforgeeks.org/user/sakshamb4oyn/" target="_blank">
+            <img src={GFGIcon} alt="GFG icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/CoolMath01" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
           <a href="https://www.linkedin.com/in/saksham-bha010/" target="_blank">
